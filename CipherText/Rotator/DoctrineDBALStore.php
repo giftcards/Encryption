ec84e2c7762ad275272778a4d4b70e28c70f7072
Jonathon Joseph <Jonathon.Joseph@bhnetwork.com>
2017-11-07 15:03:42 -0500

    DBAL Store uses transactions. Added store exceptions

--- CipherText/Rotator/DoctrineDBALStore.php
+++ CipherText/Rotator/DoctrineDBALStore.php
@@ -9,6 +9,7 @@
 namespace Giftcards\Encryption\CipherText\Rotator;
 
 use Doctrine\DBAL\Connection;
+use Exception;
 
 class DoctrineDBALStore implements StoreInterface
 {
@@ -47,25 +48,29 @@ class DoctrineDBALStore implements StoreInterface
     /**
      * @param int $offset
      * @param int $limit
-     * @return Record[]
+     * @return array
+     * @throws StoreException
      */
     public function fetch(int $offset, int $limit): array
     {
         $fields = $this->fields;
         $fields[] = $this->idField;
-        $stmt = $this->connection->createQueryBuilder()
-            ->select($fields)
-            ->from($this->table)
-            ->setFirstResult($offset)
-            ->setMaxResults($offset)
-            ->execute()
-        ;
+        try {
+            $stmt = $this->connection->createQueryBuilder()
+                ->select($fields)
+                ->from($this->table)
+                ->setFirstResult($offset)
+                ->setMaxResults($offset)
+                ->execute();
+        } catch (Exception $e) {
+            throw new StoreException("Error occurred while fetching records", $e);
+        }
 
         $records = [];
         while ($row = $stmt->fetch(\PDO::FETCH_ASSOC)) {
             new Record(
                 $row[$this->idField],
-                array_intersect_key($row,array_flip($this->fields))
+                array_intersect_key($row, array_flip($this->fields))
             );
         }
         return $records;
@@ -74,15 +79,23 @@ class DoctrineDBALStore implements StoreInterface
     /**
      * @param Record[] $rotatedRecords
      * @return void
+     * @throws StoreException
      */
     public function save(array $rotatedRecords)
     {
-        foreach ($rotatedRecords as $rotatedRecord) {
-            $this->connection->update(
-                $this->table,
-                $rotatedRecord->getData(),
-                array($this->idField => $rotatedRecord->getId())
-            );
+        $this->connection->beginTransaction();
+        try {
+            foreach ($rotatedRecords as $rotatedRecord) {
+                $this->connection->update(
+                    $this->table,
+                    $rotatedRecord->getData(),
+                    array($this->idField => $rotatedRecord->getId())
+                );
+            }
+            $this->connection->commit();
+        } catch (Exception $e) {
+            $this->connection->rollBack();
+            throw new StoreException("Error occurred while saving updated records", $e);
         }
     }
 }
--- CipherText/Rotator/StoreInterface.php
+++ CipherText/Rotator/StoreInterface.php
@@ -15,12 +15,14 @@ interface StoreInterface
      * @param int $offset
      * @param int $limit
      * @return Record[]
+     * @throws StoreException
      */
     public function fetch(int $offset, int $limit):array;
 
     /**
      * @param Record[] $rotatedRecords
      * @return void
+     * @throws StoreException
      */
     public function save(array $rotatedRecords);
 }
