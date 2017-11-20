66648b1b871c2f6e5615adbaaea5dca01093bbbb
Jonathon Joseph <Jonathon.Joseph@bhnetwork.com>
2017-11-20 14:20:44 -0500

    Reverted NullObserver

--- CipherText/Rotator/NullObserver.php
+++ CipherText/Rotator/NullObserver.php
@@ -8,21 +8,13 @@
 
 namespace Giftcards\Encryption\CipherText\Rotator;
 
-class NullObserver implements RotatorObserverInterface
+class NullObserver implements ObserverInterface
 {
-    public function fetchedRecords($offset, $limit, array $records)
+    public function rotating($id)
     {
     }
 
-    public function rotatingRecord(Record $record)
-    {
-    }
-
-    public function rotatedRecord(Record $record)
-    {
-    }
-
-    public function savedRecords($offset, $limit, array $records)
+    public function rotated($id)
     {
     }
 }
